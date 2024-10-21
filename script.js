7454c7319fb75b6c64a1494ff5e901ec9e92b062
AhmedAbou3kda <Ahmed.hussien.fcis@gmail.com>
2024-10-21 19:39:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var arr = [
+   //Use an array to hold the value of the quotes
+
+    {'author': 'Jim Rohn', 
+     'quote': 'Beware of what you become in pursuit of what you want.'
+    },
+    {'author': 'Epictetus', 
+     'quote': 'It\'s not what happens to you, but how you react to it that matters.'
+    },
+    {'author': 'Frank Sinatra', 
+     'quote': 'The best revenge is massive success.'
+    },
+    {'author': 'Wayne Gretzy', 
+     'quote': 'You miss 100% of the shots you don\'t take.'
+    },
+    {'author': 'Nelson Mandela', 
+     'quote': 'Resentment is like drinking poison and waiting for your enemies to die.'
+    },
+    {'author': 'Elbert Hubbard', 
+     'quote': 'Do not take life too seriously. You will not get out alive.'
+    },
+];
+
+function random(arrayLength)
+{
+    return Math.floor(Math.random() * arrayLength);             // Math.random() returns a random number between 0 (inclusive),  and 1 (exclusive).
+}
+
+function generateRandomQuote()
+{
+    var randomNumber = random(arr.length);                   // 0 to 14
+    document.getElementById("quote").innerHTML = arr[randomNumber].quote;
+    document.getElementById("author").innerHTML = "-- " + arr[randomNumber].author;
+}
